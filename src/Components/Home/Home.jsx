0a381e809210b9f4c2fb78ffef9802466853ca09
Ahmedsamehm <a7medxd992@hotmail.com>
2024-08-19 00:38:41 +0300

    fix:every thing

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,24 +10,19 @@ import Image2 from "../../images/XCM_Manual_1533480_5305769_379x304_1X._SY304_CB
 
 export default function Home() {
   const { products, isLoading, setIsLoading } = useContext(ProductsContext);
-
   useEffect(() => {
     setIsLoading(true);
     setTimeout(() => {
       setIsLoading(false);
-    }, 50);
-  }, []);
-
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 50);
+    }, 900);
   }, []);
 
   return (
     <>
-      <section id="home" className="container mx-auto px-4 py-4">
+      {
+        isLoading ? <Loading /> : 
+          
+        <section id="home" className="container mx-auto px-4 py-4">
         <div className="flex flex-col lg:flex-row  md:flex-row  items-center  gap-3 md:px-6">
             <HomeTopSlider />
           <div className="w-full lg:w-1/3  md:w-1/3   space-y-4 flex flex-col items-center mx-auto ">
@@ -44,9 +39,6 @@ export default function Home() {
           </div>
         </div>
 
-        {isLoading ? (
-          <Loading />
-        ) : (
           <div className="mt-8">
               <ProductSlider />
               
@@ -56,8 +48,11 @@ export default function Home() {
               ))}
             </div>
           </div>
-        )}
+ 
       </section>
+          
+      }
+  
     </>
   );
 }
--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -14,10 +14,12 @@ export default function ProductDetails() {
 
   const [ProductDetails, setProductDetails] = useState(null);
   const { isLoading, setIsLoading } = useContext(ProductsContext);
-  const {UserToken}=useContext(AuthContext)
-  
+  const { UserToken } = useContext(AuthContext);
+
   useEffect(() => {
+
     GetProductDetails();
+  
   }, []);
 
   async function GetProductDetails() {
@@ -27,59 +29,62 @@ export default function ProductDetails() {
     );
     setProductDetails(data.data);
     setIsLoading(false)
-    
-
-
   }
 
   return (
     <>
+      {isLoading ? <Loading /> : 
+      
       <section id="ProductDetails">
-      {
-        isLoading ? <Loading /> :
-        <div className="bg-gray-100 my-5 p-10">
-        <div className="container mx-auto px-4 py-8">
-          <div className="flex flex-wrap -mx-4">
-            <div className="w-full md:w-1/2 px-4 mb-8 ">
+      
+      <div className="bg-gray-100 my-2 sm:my-3 md:my-4 lg:my-5 p-4 sm:p-6 md:p-8 lg:p-10">
+        <div className="container mx-auto px-2 sm:px-3 md:px-4 py-4 sm:py-6 md:py-8">
+          <div className="flex flex-col md:flex-row -mx-2 md:-mx-4">
+            <div className="w-full md:w-1/2 px-2 md:px-4 mb-4 md:mb-0">
               <img
-                src={ProductDetails?.imageCover }
+                src={ProductDetails?.imageCover}
                 alt="Product"
-                className="md:w-5/6 lg:w-96 mx-auto rounded-lg shadow-md mb-4"
+                className="w-full md:w-5/6 lg:w-4/5 xl:w-3/4 mx-auto rounded-lg shadow-md mb-4"
                 id="mainImage"
-                  />
-                  <div className="lg:w-full  mx-auto">
-                  <DetailsSlider images={ProductDetails?.images} />
-                  </div>
-               
+              />
+              <div className="w-full lg:w-5/6 xl:w-4/5 mx-auto">
+                <DetailsSlider images={ProductDetails?.images} />
+              </div>
             </div>
 
-
-            <div className="w-full md:w-1/2 px-4">
-              <h2 className="text-3xl font-bold mb-2">
-                {ProductDetails?.title }
+            <div className="w-full md:w-1/2 px-2 md:px-4">
+              <h2 className="text-2xl sm:text-3xl font-bold mb-2">
+                {ProductDetails?.title}
               </h2>
-              <p className="text-gray-600 mb-4">SKU: WH1000XM4</p>
-              <div className="mb-4">
-                <span className="text-2xl font-bold mr-2">${ProductDetails?.price }</span>
+              <p className="text-gray-600 mb-2 sm:mb-4">SKU: WH1000XM4</p>
+              <div className="mb-2 sm:mb-4">
+                <span className="text-xl sm:text-2xl font-bold mr-2">
+                  ${ProductDetails?.price}
+                </span>
                 <span className="text-gray-500 line-through">$399.99</span>
               </div>
-              <div className="flex items-center mb-4 ">
-                  <RatingProducts rating={ProductDetails?.ratingsAverage} />
-                <span className="ml-2 text-gray-600 "> ({ProductDetails?.ratingsQuantity} reviews)</span>
+              <div className="flex items-center mb-2 sm:mb-4">
+                <RatingProducts rating={ProductDetails?.ratingsAverage} />
+                <span className="ml-2 text-gray-600 text-sm sm:text-base">
+                  ({ProductDetails?.ratingsQuantity} reviews)
+                </span>
               </div>
-              <p className="text-gray-700 mb-6">
-                {ProductDetails?.description }
+              <p className="text-gray-700 mb-4 sm:mb-6 text-sm sm:text-base">
+                {ProductDetails?.description}
               </p>
-         
-              <div className="flex space-x-4 mb-6">
-                <button onClick={()=>AddToCart(ProductDetails._id,UserToken)} className="bg-indigo-600 flex gap-2 items-center text-white px-6 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+
+              <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 md:space-x-4 mb-4 sm:mb-6">
+                <button
+                  onClick={() => AddToCart(ProductDetails._id, UserToken)}
+                  className="w-full sm:w-auto bg-indigo-600 flex justify-center items-center gap-2 text-white px-3 sm:px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 text-sm md:text-base"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
-                    className="size-6"
+                    className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
                   >
                     <path
                       strokeLinecap="round"
@@ -89,14 +94,19 @@ export default function ProductDetails() {
                   </svg>
                   Add to Cart
                 </button>
-                <button onClick={()=>AddToWishList(ProductDetails._id,UserToken)} className="bg-gray-200 flex gap-2 items-center  text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+                <button
+                  onClick={() =>
+                    AddToWishList(ProductDetails._id, UserToken)
+                  }
+                  className="w-full sm:w-auto bg-gray-200 flex justify-center items-center gap-2 text-gray-800 px-3 sm:px-4 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 text-sm md:text-base"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
-                    className="size-6"
+                    className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
                   >
                     <path
                       strokeLinecap="round"
@@ -108,19 +118,21 @@ export default function ProductDetails() {
                 </button>
               </div>
               <div>
-                <h3 className="text-lg font-semibold mb-2">More Details</h3>
-                <ul className="list-disc list-inside text-gray-700">
-                  <li>{ProductDetails?.brand.name }</li>
+                <h3 className="text-base sm:text-lg font-semibold mb-2">
+                  More Details
+                </h3>
+                <ul className="list-disc list-inside text-gray-700 text-sm sm:text-base">
+                  <li>{ProductDetails?.brand.name}</li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+  </section>
       }
-      </section>
     
-     
     </>
   );
 }
